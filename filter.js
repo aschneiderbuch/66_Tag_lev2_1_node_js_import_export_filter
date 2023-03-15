05e3f63b9dbe5aa1563d6ff99eec2ce387137d8a
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-03-15 23:48:19 +0100

    funktion mehr als

--- filter.js
+++ filter.js
@@ -2,4 +2,32 @@
 
 import { data } from "./cities.js";
 
-console.log(data)
+/* funktion 1
+rückgabe     Städte mit   mehr   als 100 000 Einwohner */
+
+function mehrEinwohnerAls(paraZahl, inputArrMitObj) {
+    let z = Number(paraZahl)
+    let arrObj = inputArrMitObj
+
+    let ergebniss = []
+    arrObj.map((i) => {
+        if (i.population >= z) {
+            return ergebniss.push(i)
+        }
+        /*      else {
+                 
+            }  */
+    })
+    return ergebniss
+
+}
+
+
+
+
+/* funktion 
+rückgabe     Städte mit   weniger   als 100 000 Einwohner */
+
+
+
+export { mehrEinwohnerAls }
--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 /* zusammen führen */
+
+import { mehrEinwohnerAls } from "./filter.js";
+import { data } from "./cities.js";
+
+
+console.log(mehrEinwohnerAls(900000, data))
